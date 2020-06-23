e19b503465e3f87b0f27f526f94df8cb1947f400
ssathiyan <sathiyans7@gmail.com>
2020-06-23 23:37:17 +0530

    Added SparkConfig.scala #Common spark session object to be used in all spark programs

---
+++ src/main/scala/com/styn/utils/SparkConfig.scala
@@ -0,0 +1,13 @@
+package com.styn.utils
+
+import org.apache.spark.sql.SparkSession
+
+/**
+ * Created by STYN on 23-06-2020
+ */
+object SparkConfig {
+  val spark = SparkSession.builder().master("local").config("spark.driver.host","localhost").getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  val hdfs = spark.sparkContext.hadoopConfiguration
+}
